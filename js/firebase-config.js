2d457d33dcd2fd05bf6e84f4289dd6164a0cc671
remotvgame <remotvgame@gmail.com>
2025-07-30 11:27:36 +0300

    Update and rename script.js to js/firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCYIy-cKJBEdqHcF_MdLHjv8FWJgYjkR5w",
+  authDomain: "depi-5a814.firebaseapp.com",
+  databaseURL: "https://depi-5a814-default-rtdb.firebaseio.com",
+  projectId: "depi-5a814",
+  storageBucket: "depi-5a814.firebasestorage.app",
+  messagingSenderId: "576108269318",
+  appId: "1:576108269318:web:63931280c74bcebb455887"
+};
